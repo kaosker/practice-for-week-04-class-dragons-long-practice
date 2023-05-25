b721e7756ed8098e794830bd07ba4dd91dc6405b
kaosker <thorup93@hotmail.com>
2023-05-25 06:58:11 +0200

    final product

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,19 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name);
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+}
+
+module.exports = Dragon;
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
